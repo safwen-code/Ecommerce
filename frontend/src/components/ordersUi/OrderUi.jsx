fa8659c731454f6ab4144ee8684b9d3cbcb8e00a
safwen-code <safwendjebbi1234@gmail.com>
2025-12-24 23:08:05 +0100

    add order product user components

---
+++ frontend/src/components/ordersUi/OrderUi.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const OrderUi = () => {
+  return <div>OrderUi</div>
+}
+
+export default OrderUi
---
+++ frontend/src/components/productsUi/ProductUi.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const ProductUi = () => {
+  return <div>ProductUi</div>
+}
+
+export default ProductUi
