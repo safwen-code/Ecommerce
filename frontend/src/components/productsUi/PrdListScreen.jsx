547bf2b1f6987c2a83e7c38884f19ea6c0cdb1e3
safwen-code <safwendjebbi1234@gmail.com>
2025-12-26 20:28:30 +0100

    add UI product Screen

--- frontend/src/components/productsUi/PrdListScreen.jsx
+++
@@ -1,9 +0,0 @@
-///admin/productlist
-// "/admin/productlist/:pageNumber"
-import React from 'react'
-
-const PrdListScreen = () => {
-  return <div>PrdListScreen</div>
-}
-
-export default PrdListScreen
