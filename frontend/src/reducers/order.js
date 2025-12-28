6c9a84b5dbb51581f5bc61648092e6d782f8a36c
safwen-code <safwendjebbi1234@gmail.com>
2025-12-28 18:53:23 +0100

    create the Base reducers for project

--- frontend/src/reducers/order.js
+++ frontend/src/reducers/order.js
@@ -1,1 +1,27 @@
 // order reducer
+
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+  order: {},
+  orders: [],
+  loading: false,
+  success: false,
+  error: null,
+}
+const orderSlice = createSlice({
+  name: 'order',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+    // create Order
+    // order Details
+    // pay Order
+    // list Order
+    // My List Order
+    // Deliver order
+  },
+})
+
+export const order = orderSlice.reducer
