27e1937d64641d72cab98e5b9837edd7da58edca
safwen-code <safwendjebbi1234@gmail.com>
2025-12-28 18:53:23 +0100

    create the Base reducers for project

--- frontend/src/reducers/user.js
+++ frontend/src/reducers/user.js
@@ -1,1 +1,30 @@
 // user reducer
+
+//loginuser  : userInfo
+//registerUser : userInfo
+// userDetail : user
+// userUpdate: userInfo
+// listUser: users
+// userUodate :user
+
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+  userInfo: {},
+  user: {},
+  users: [],
+  loading: false,
+  error: null,
+  success: false,
+}
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+  },
+})
+
+export const user = userSlice.reducer
