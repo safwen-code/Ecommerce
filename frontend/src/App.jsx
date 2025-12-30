c6d520134099c4dbe5abc121f6343e9fc5192e7e
safwen-code <safwendjebbi1234@gmail.com>
2025-12-30 22:29:47 +0100

    use routes to hundel pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,18 @@
 import HomeScreen from './components/HomeScreen'
-import { Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import PrdListScreen from './components/productsUi/PrdListScreen'
+import PrdUpdateScreen from './components/productsUi/PrdUpdateScreen'
+import PrdDetail from './components/productsUi/prdDetail'
 const App = () => {
   return (
     <>
       <Router>
-        <Route exact path="/" element={<HomeScreen />} />
+        <Routes>
+          <Route exact path="/" element={<HomeScreen />} />
+          <Route exact path="/ListPrd" element={<PrdListScreen />} />
+          <Route exact path="/UpdatePrd" element={<PrdUpdateScreen />} />
+          <Route exact path="/Prdbyid" element={<PrdDetail />} />
+        </Routes>
       </Router>
     </>
   )
