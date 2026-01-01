b27e181d1f404bdd511ac7aa6a9d1a9f21de8312
safwen-code <safwendjebbi1234@gmail.com>
2026-01-01 17:40:08 +0100

    add users screen

--- frontend/src/components/usersUi/UserLogin.jsx
+++ frontend/src/components/usersUi/UserLogin.jsx
@@ -1,7 +1,91 @@
-import React from 'react'
+import {
+  Container,
+  TextField,
+  Typography,
+  Box,
+  Button,
+  Grid,
+  Link,
+  Paper,
+} from '@mui/material'
+import { useState } from 'react'
 
 const UserLogin = () => {
-  return <div>UserLogin</div>
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+
+    console.log('login')
+  }
+
+  return (
+    <Container maxWidth="xs">
+      <Paper elevation={3} sx={{ padding: 4, marginTop: 8, borderRadius: 2 }}>
+        <Box
+          component="form"
+          onSubmit={submitHandler}
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Typography
+            component="h1"
+            variant="h5"
+            sx={{ mb: 3, fontWeight: 'bold' }}
+          >
+            Login
+          </Typography>
+
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="البريد الإلكتروني"
+            name="email"
+            autoComplete="email"
+            autoFocus
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="كلمة المرور"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2, py: 1.2 }}
+          >
+            Register
+          </Button>
+
+          <Grid container>
+            <Grid item xs>
+              <Typography variant="body2">
+                do you have acount ?<Link underline="hover">Register</Link>
+              </Typography>
+            </Grid>
+          </Grid>
+        </Box>
+      </Paper>
+    </Container>
+  )
 }
 
 export default UserLogin
